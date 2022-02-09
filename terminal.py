24803d2909be353ea52b92710709bcc3ab9aa8b3
F/Poutre <kevin@fpout.re>
2022-02-09 16:18:47 +0100

    2nd Implementation

--- terminal.py
+++ terminal.py
@@ -1,22 +1,43 @@
 import argparse
 import serial
 import sys
-import signal
-
-from serialReader import SerialReader
-from serialWriter import SerialWriter
+from threading import Thread
 
 
-def cleanup(sig, frame):
-    global ser, serialReader, serialWriter
-    print("Closing Serial Connection...")
-    ser.close()
-    print("Done.")
-    print("Waiting for all threads to stop...")
-    serialReader.join()
-    serialWriter.join()
-    print("Done. Goodbye !")
-    sys.exit(0)
+class SerialReader(Thread):
+
+    def __init__(self, serial):
+        Thread.__init__(self)
+        self.serial = serial
+        self.stop = False
+    
+    def kill(self):
+        self.stop = True
+    
+    def run(self):
+        while not self.stop:
+            try:
+                print(self.serial.readline().decode())
+            except KeyboardInterrupt:
+                break
+
+class SerialWriter(Thread):
+    def __init__(self, serial):
+        Thread.__init__(self)
+        self.serial = serial
+        self.stop = False
+    
+    def kill(self):
+        self.stop = True
+    
+    def run(self):
+        while not self.stop:
+            try: 
+                command = input('>')
+                command += '\n'
+                self.serial.write(command.encode())
+            except KeyboardInterrupt:
+                break
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
@@ -35,10 +56,9 @@ if __name__=='__main__':
     ser = serial.Serial(args.dev, args.baudrate)
     serialReader = SerialReader(ser)
     serialWriter = SerialWriter(ser)
-    ser.open()
     serialReader.start()
     serialWriter.start()
 
-    signal.signal(signal.SIGINT, cleanup)
-    signal.pause()
-
+    serialReader.join()
+    serialWriter.join()
+    sys.exit(0)
